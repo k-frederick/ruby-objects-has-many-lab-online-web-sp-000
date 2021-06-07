48bd0171c85d7e3f16d20e0c63d7419f9b4045e8
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-07 03:38:37 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,8 +9,8 @@ class Author
   def add_post(post)
     post.author = self
   end
-  def add_post_by_title(post)
+  def add_post_by_title(name)
     post = Post.new(name)
-    add_post(post)
+		add_post(post)
   end
 end
