cfefccffc01277ad9129ba200104a4eb9a76ccc5
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-07 03:16:07 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,37 @@
+# class Post
+#   attr_accessor :title, :author
+#   @@all = []
+#   def initialize(title)
+#     @@all << self
+#   end
+#   def self.all
+#     @@all
+#   end
+#   def title
+#     #title
+#   end
+# 
+# end
 class Post
   attr_accessor :title, :author
+
   @@all = []
+
   def initialize(title)
+    @title = title
     @@all << self
   end
+
   def self.all
     @@all
   end
-  def title
-    #title
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
   end
 
 end
