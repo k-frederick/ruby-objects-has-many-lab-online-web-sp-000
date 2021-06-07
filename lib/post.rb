2e6cd4991db8341a604beaf1bf3ae54d65089f71
Kristina Frederick <kristinafrederick@icloud.com>
2021-06-07 03:18:39 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,17 +1,3 @@
-# class Post
-#   attr_accessor :title, :author
-#   @@all = []
-#   def initialize(title)
-#     @@all << self
-#   end
-#   def self.all
-#     @@all
-#   end
-#   def title
-#     #title
-#   end
-#
-# end
 class Post
   attr_accessor :title, :author
 
